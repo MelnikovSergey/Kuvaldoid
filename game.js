70cb9420adc574be7ff55fe9139fb83e6e3d9ee5
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2018-11-16 10:29:22 +0300

    Update game.js
    getContext and drawImage

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
 //single global variable in the game
 var game = {
 	start: function(){
+		ctx: undefined;
+		sprites: {
+			background: undefined;
+		},
+		var canvas = document.getElementById('gameArea');
+		this.ctx = canvas.getContext('2d');
 
+		this.sprites.background = new Image();
+		this.sprites.background.src = 'images/background.png';
+
+		this.ctx.drawImage(this.sprites.background, 0, 0)
 	}
 };
 
