18e4fe4cdac22e4d395986111b7a1eefcc82925f
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2018-11-16 11:36:51 +0300

    Update game.js
    Start drawing assets (second try), game loop.

--- game.js
+++ game.js
@@ -3,21 +3,26 @@ var game = {
 	ctx: undefined,
 	sprites: {
 		background: undefined,
-	},	
+	},
 	start: function(){
-		var canvas = document.getElementById('gameArea');
-		this.ctx = canvas.getContext('2d');
+		var canvas = document.getElementById("gameArea");
+		this.ctx = canvas.getContext("2d");
 
 		this.sprites.background = new Image();
 		this.sprites.background.src = 'images/background.png';
 
-		window.requestAnimationFrame(function(){
-
-		})
+		this.run();
 	},
 	render: function(){
-		this.ctx.drawImage(this.sprites.background, 0, 0)
+		this.ctx.drawImage(this.sprites.background, 0, 0);
 	},
+	run: function(){
+		this.render();
+
+		window.requestAnimationFrame(function(){
+			game.run();
+		});		
+	}
 };
 
 // start point, checking page load
