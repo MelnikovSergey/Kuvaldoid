69ca9d7922a65df28912e84b320dc50a3b0e7986
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2018-11-17 20:18:59 +0300

    Update game.js
    Draw platform img

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var game = {
 	ctx: undefined,
 	sprites: {
 		background: undefined,
+		platform: undefined
 	},
 	start: function(){
 		var canvas = document.getElementById("gameArea");
@@ -11,10 +12,14 @@ var game = {
 		this.sprites.background = new Image();
 		this.sprites.background.src = 'images/background.png';
 
+		this.sprites.platform = new Image();
+		this.sprites.platform.src = 'images/platform.png';		
+
 		this.run();
 	},
 	render: function(){
 		this.ctx.drawImage(this.sprites.background, 0, 0);
+		this.ctx.drawImage(this.sprites.platform, 0, 0);		
 	},
 	run: function(){
 		this.render();
