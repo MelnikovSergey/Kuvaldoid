922d5c63267f32d0dab279daa4cbb9f63bb66452
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2018-11-18 23:08:17 +0300

    Update game.js
    add Blocks (level design)

--- game.js
+++ game.js
@@ -4,11 +4,15 @@ var game = {
 	height: 360,
 	ctx: undefined,
 	platform: undefined,
-	ball: undefined,		
+	ball: undefined,
+	rows: 4,
+	cols: 8,
+	blocks: [],		
 	sprites: {
 		background: undefined,
 		platform: undefined,
-		ball: undefined
+		ball: undefined,
+		block: undefined
 	},
 	init: function() {
 		var canvas = document.getElementById("gameArea");
@@ -20,9 +24,21 @@ var game = {
 			this.sprites[key].src = 'images/' + key + '.png';
 		}
 	},
+	create: function(){
+		//level blocks
+		for (var col=0, col < this.cols; col++) {
+			this.blocks.push({
+				x: 68 * col,
+				y: 0,
+				width: 64,
+				height: 32
+			});
+		}
+	},
 	start: function(){
 		this.init();
 		this.load();
+		this.create();		
 		this.run();
 	},
 	render: function(){
@@ -30,6 +46,7 @@ var game = {
 		this.ctx.drawImage(this.sprites.background, 0, 0);
 		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
 		this.ctx.drawImage(this.sprites.ball, this.ball.width * this.ball.frame, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);				
+		this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
 	},
 	run: function(){
 		this.render();
@@ -45,7 +62,7 @@ game.ball = {
 	height: 22,
 	frame: 0,
 	X: 340,
-	y: 270
+	y: 278
 };
 
 game.platform = {
